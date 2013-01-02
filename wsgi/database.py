494ddd7fc38acbe2c99a9dcf86bdb4d67ca4e358
zhengfa dang <dantifer@gmail.com>
2013-01-02 11:38:02 +0800

    update database_url in openshift

--- wsgi/database.py
+++ wsgi/database.py
@@ -10,8 +10,7 @@ import os
 # OPENSHIFT_MYSQL_DB_URL="mysql://admin:6bbgzDwESXPL@127.6.27.1:3306/"
 # OPENSHIFT_MYSQL_DB_USERNAME="admin"
 if 'OPENSHIFT_APP_UUID' in os.environ:
-    # database_url = '%spython' % (os.environ['OPENSHIFT_MYSQL_DB_URL'])
-    database_url = 'mysql://admin:m1Ua-Cg8MJqL@da531a6d91-joylin.rhcloud.com:47561/demo'
+    database_url = '%s%s' % (os.environ['OPENSHIFT_MYSQL_DB_URL'], os.environ['OPENSHIFT_APP_NAME'])
 else:
     database_url = 'mysql://root@127.0.0.1/fwmrm_oltp_zfdang'
 engine = create_engine(database_url, encoding='UTF-8', echo=True)
