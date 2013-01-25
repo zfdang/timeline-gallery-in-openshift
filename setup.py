085e0087b61c49f01cff5b6caa47f7835862d3c1
zhengfa dang <dantifer@gmail.com>
2013-01-25 15:55:05 +0800

    update readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='www.zfdang.com',
       version='1.0',
-      description='application to show openshift capabilities',
+      description='timeline gallery in openshift',
       author='Zhengfa DANG',
       author_email='dantifer@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
